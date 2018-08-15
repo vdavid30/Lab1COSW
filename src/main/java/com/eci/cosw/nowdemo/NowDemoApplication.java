33e93da624399f971fc269154b91f99b71f9f2ff
David Vaca <spm1dh@gmail.com>
2018-08-15 17:49:37 -0500

    basic commit

---
+++ src/main/java/com/eci/cosw/nowdemo/NowDemoApplication.java
@@ -0,0 +1,12 @@
+package com.eci.cosw.nowdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NowDemoApplication.class, args);
+	}
+}
